0d52234f3a408c2ebcd1ea6b55736083fd415636
JJSEONG <hans3238@naver.com>
2022-06-24 20:43:20 +0900

    항해99-프로그래머스 알고리즘: 2번 문제

--- 2.divisor-number.js
+++ 2.divisor-number.js
@@ -4,7 +4,7 @@
 /* [문제]
 array의 각 element 중 divisor로 나누어 떨어지는 값을 오름차순으로 정렬한 배열을 반환하는 함수, ssolution을 작성해주세요. divisor로 나누어 떨어지는 element가 하나도 없다면 배열에 -1을 담아 반환하세요.*/
 
-function solution(arr, divisor) {
+function solution1(arr, divisor) {
   var answer = [];
   for (i = 0; i < arr.length; i++) {
     if ((arr[i] % divisor) === 0) {
@@ -25,8 +25,7 @@ console.log(solution([5, 10, 15, 20, 23], 5))
 
 // Anothoer Code [삼항연산자 사용]//
 
-/*
-function solution(arr, divisor) {
+function solution2(arr, divisor) {
   var answer = [];
   for (i = 0; i < arr.length; i++) {
     if ((arr[i] % divisor) === 0) {
@@ -36,4 +35,3 @@ function solution(arr, divisor) {
 
   return answer.length === 0? [-1]:answer.sort((a,b) => a - b)
 }
-*/
