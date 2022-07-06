e35d943377f205ad45a6ef2c19f3deaa12fb51ad
JJSEONG <hans3238@naver.com>
2022-07-06 21:48:27 +0900

    항해99-프로그래머스 알고리즘: 31번 문제

--- 31.find_prime_num.js
+++ 31.find_prime_num.js
@@ -59,17 +59,28 @@ function solution(n) {
       // numbers[2] 부터 시작하므로, 2 는 0 이 아니므로 아래 for문으로 이동하게 된다.
       if (numbers[i] === 0) continue;
 
-      for (let j = 2 * i; j <= numbers.length; j += i){   
+      // for문을 돌며, j 가 i * 2 로 시작을 하고, j 가 += 2가 증가하게 된다.
+      // j 가 i * 2 로 시작하는 이유는 i 의 숫자가 2 부터 시작을 하게 되고,
+      // 2 * 2 = 4 가 출력이 된다. 그러므로 4는 2의 배수로 소수가 될 수 없기 때문에 0으로 지정한다.
+      // 그 후 j 에 i 의 값을 더해서 올려주면 j 의 값은 + 2씩 증가하게 되고,
+      // 아래의 함수는 처음엔 2의 배수를 만나면, 0으로 변환시켜주는 내용의 반복문이다.
+      // 그 후, i 의 값이 증가하면서 3의 배수 / 4의 배수 / 5의 배수 ... 넘어가면서
+      // 소수가 아닌 숫자들은 전부 0 으로 변환해주는 내용이 된다.
+      for (let j = 2 * i; j <= numbers.length; j += i){
+          console.log(`J = ${j}`)
           numbers[j] = 0;
       }
       console.log(`i의 숫자 : ${i} | 배열 : ${numbers}`)
   }
 
-  // for(let i = 2; i < numbers.length; i++){
-  //     if(numbers[i]!=0) answer.push(numbers[i])
-  // }
-
-  // return answer.length
+  // 위에서 걸러진 배열을 가지고와, i가 2부터 시작하는 반복문을 가지고온다.
+  for(let i = 2; i < numbers.length; i++){
+      // 만약 numbers[i]의 값이 0이 아니라면 answer의 배열에 넣어줘!
+      if(numbers[i]!==0) answer.push(numbers[i])
+  }
+  
+  // 그 후, answer의 길이를 출력하면 이 문제의 답이 출력이된다.
+  return answer.length
 }
 
-console.log(solution(10))
+console.log(solution(20))
